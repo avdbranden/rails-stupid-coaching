d01545113d6a902ef895cc5015cfa66eca9fbd92
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-12 11:38:52 +0200

    destroy pages controller/views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/ask', to: 'pages#ask'
-
-  root 'pages#ask'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
