59292afaa3dce9db7a48db8fbccdd9bcfcb60782
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-12 12:14:21 +0200

    implement stupid coaching logic, including upcase and link disappearing

--- app/controllers/coachings_controller.rb
+++ app/controllers/coachings_controller.rb
@@ -1,5 +1,10 @@
 class CoachingsController < ApplicationController
   def answer
+    query = params[:query]
+    @response = "I don't care, get dressed and go to work!"
+    @response = "Silly question, get dressed and go to work!" if query.end_with?("?")
+    @response = "#{@response} I can feel your motivation!" if query.upcase
+    @response = nil if query == "I am going to work right now!"
   end
 
   def ask
