d71728a9812abe8d3bed1352849659211be2062a
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-12 11:23:58 +0200

    configure root to pages#ask

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/ask'
+  get 'pages/ask', to: 'pages#ask'
+
+  root 'pages#ask'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
